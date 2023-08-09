b60c57a617f17235886d2ab182577c11b473969b
Vladyslav Horovyi <vladyslav.horovyi.dev@gmail.com>
2023-08-09 12:04:43 +0200

    amp

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+let counter = 0;
+
+for (const menu of document.querySelectorAll(".menu")) {
+  menu.addEventListener("click", () => {
+      menu.querySelector(".menu-text").classList.toggle("menu-text-open");
+      menu.querySelector(".list_titles").classList.toggle("active_menu");
+      if (counter % 2 === 0) {
+        menu.classList.add("active");
+      } else {
+        menu.classList.remove("active");
+      }
+      counter = counter + 1;
+      menu.querySelector(".menu-content").classList.toggle(
+          "menu-content-hidden"
+      );
+      console.log(counter)
+  });
+}
